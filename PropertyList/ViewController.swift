b74998d97687ebed2d7febf105f142fdafa3ffc3
owner <canerduru@ymail.com>
2021-02-01 19:58:35 +0300

    Initial Commit

---
+++ PropertyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PropertyList
+//
+//  Created by owner on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
