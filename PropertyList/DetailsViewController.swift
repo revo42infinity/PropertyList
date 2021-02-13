abf5eeef24a51b14a7fb83357aecdfe640933c4f
owner <canerduru@ymail.com>
2021-02-13 17:32:33 +0300

    coredata, save, pick image

--- PropertyList/DetailsViewController.swift
+++ PropertyList/DetailsViewController.swift
@@ -18,9 +18,81 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     @IBOutlet weak var priceTextField: UITextField!
     
+    //27 secilen urun neyse bilgilerini aktarmak icin yazmaya basliyoruz.bu sebeple iki degisken olusutuyoruz
+    
+    var selectedType = ""
+    var selectedId : UUID?
+    
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //28
+        if selectedType != "" { //eger type secildiyse yani bos degil ise
+            //core data secilen urun bilgilerini goster.ordan cekip goster
+            
+            //33 sadece tiklanan urunun uuidsini diger tarafta logda aliyoruz. string olarak tiklananlarin uuid sini aliyoruz boylece
+            if let uuidstring = selectedId?.uuidString{
+              //35 print(uuidstring)
+                
+                //35uuid gelirse ne yapacagiz? onu yazalim
+                let appdelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appdelegate.persistentContainer.viewContext
+                
+                //36 cekme islemi icin
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Properties")
+                
+                //38 filte ekleyecegiz. mantiksal sinirlar koy arama buna gore yapilacak demek predicate. format istiyor. neyi filtrelemek istiyoruz?? ID yi
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidstring) //id si uuidstring(yada ne isim yazarsan buna) e esit olanlari getir %@ bunun anlami...
+                
+                //37
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                //39
+                do {
+                    let results = try context.fetch(fetchRequest)
+                    if results.count > 0 {
+                        for result in results as! [NSManagedObject]{
+                            //image view textview a yazdiricam
+                            if let type = result.value(forKey: "type") as? String {
+                                typeTextField.text = type
+                                
+                            }
+                            if let price = result.value(forKey: "price") as? Int {
+                                priceTextField.text = String(price)
+                            }
+                            if let location = result.value(forKey: "location") as? String {
+                                locationTextField.text = location
+                            }
+                            //gorselin datasini alacagiz. bunu sonra uiimage a cevirecegiz .
+                            if let pictData = result.value(forKey: "pictures") as? Data {
+                                let image = UIImage(data: pictData) //uiimage olsturacak
+                                imageView.image = image
+                            }
+                        }
+                    }
+                } catch {
+                    print("error")
+                    
+                }
+                
+                
+            }
+            
+            
+            
+            
+        } else { //demekki yukaridaki artiya tiklayrak geldi secerek gelmedi urunu
+            //bos gosterelim
+            typeTextField.text = ""
+            locationTextField.text = ""
+            priceTextField.text = ""
+            
+            
+            
+        }
 
         //4 keyboard kapama bir yere tiklaninca. once bir yere tiklanmayi algilama
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
@@ -48,7 +120,7 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     //11 gorseli sectin sonra nolacak
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        imageView.image = info[.originalImage] as? UIImage //editing e izin ver ya da orjinali ver vs
+        imageView.image = info[.originalImage] as? UIImage //editing e izin ver ya da orjinali ver vs...any verdigi icin as ile cast ettim. opsiyonel cast
         self.dismiss(animated: true, completion: nil) //picker controller kapa, imageview a geri don
         //12 infolist ten aciklama ekle image choose description icin
     }
@@ -61,11 +133,11 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     @IBAction func saveButton(_ sender: Any) {
-        //13 appdelegate da context vs yazacagimiz icin appdelegate i degisken yapmak lazim once
+        //13 appdelegate da context vs yazacagimiz icin appdelegate i degisken yapmak lazim once. CORE Data icin aynisini yazdik.
         let appDelegate = UIApplication.shared.delegate as! AppDelegate //appdelegate i degisken olarak tanimliyoruz
         let context = appDelegate.persistentContainer.viewContext
         
-        //14
+        //14 13 u yaptik cunku liste kaydetmeye calisiyoruz...coredate modeli icin nsentity yapiyoruz. import coredata yaptik.
         let houses = NSEntityDescription.insertNewObject(forEntityName: "Properties", into: context)
         houses.setValue(typeTextField.text, forKey: "type") //anahtar kelime icin deger tanimla. anahtar kelime attribute lar
         houses.setValue(locationTextField.text, forKey: "location")
@@ -85,11 +157,20 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         //17
         do {
-            try context.save() //throws gordugumuz icin do try catch yap
+            try context.save() //sadece context.save yaptigimizda throws gordugumuz icin do try catch yap
         } catch {
             print("error")
             
         }
+        //26 diger tarafa gecmeden once veri kaydettigimizi diger controllere haber vermek istiyorum. haber yollamak istedigimizde notfiicatin center kulllaniriz
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "DataSaved"), object: nil) //ne mesaji yollayacagiz
+        
+        
+        
+        
+        //25 viewcontrole geri donmek lazim kaydettikten sonra
+        self.navigationController?.popViewController(animated: true)
+        
         
     }
     
--- PropertyList/ViewController.swift
+++ PropertyList/ViewController.swift
@@ -6,24 +6,144 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-    @IBOutlet weak var viewController: UITableView!
+//18
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //22 iki dizi olsuturucaz. cunku tableview da sadece id ve type cekecegiz.hafizayi dogru kullanmak icin herseyi burada cekmeyecegiz diger tarafta zaten yapacagiz
+    var typeDizi = [String]()
+    var idDizi = [UUID]()
+    
+    //29
+    var selecttype = ""
+    var selectUUID : UUID?
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        
+        
         
         
         //1 nav barda arti button
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addbuttonclicked))
+        
+        //22
+        takeData()
+        
     }
+    
+    //26 gorunum gosterilmeden once cagriliyor
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(takeData), name: NSNotification.Name?(NSNotification.Name(rawValue: "DataSaved")), object: nil) //gozlemci ekleyecegiz/notification centerde gozlemcilere haber veriyor cunku
+        //bildirim gelirse nae yapacak? selector..ordaki func da take Data olacak
+    }
+    
+    
+    //19
+    @objc func takeData() {
+        //27 detailsVC de kaydedince oncekileri tekrar aldi kaydetti iki kere kayi olmamasi icin yapiyoruz bunu
+        typeDizi.removeAll(keepingCapacity: false)
+        idDizi.removeAll(keepingCapacity: false)
+        //19 devam
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //veri cekme istegi yapmamiz lazim...istegi degisken olarak tanimlamak lazim
+        //20
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Properties")
+        fetchRequest.returnsObjectsAsFaults = false
+        //veriler buyuk olunca false olsun
+        
+        //21
+        do {
+            let results = try context.fetch(fetchRequest)
+            //34
+            if results.count > 0 { //sonuclar sifirdan buyukse bu islemleri yap diyoruz
+                //21
+                for result in results as! [NSManagedObject]  {//cast ediyoruz nsmangedobeect olarak...coredatadan gelecgni biliyoruz
+                    
+                    
+                    //23 if let yaptik let type = result.value(forKey: "type") as? String
+                    if let type = result.value(forKey: "type") as? String {
+                        typeDizi.append(type)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idDizi.append(id)
+                        
+                    }
+                }
+                
+                //24 datalari degistrdik guncelle dememiz lazim
+                tableView.reloadData()
+                
+            }
+            
+            
+        }
+                catch {
+                    print("error")
+                    
+                }
+            }
+        
+    
+    
+    
 //2
     @objc func addbuttonclicked() {
+        //32 secilen type bos string olduguna emin olurska her arti butona tiklandiginda bos string olarak gidecek diger tarafta da bos gelince detailsvc anlayacak. giderken bos string gidecek yani
+        selecttype = ""
+        
         //3
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
         
     }
-
+    
+    //19
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return typeDizi.count     //24
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+       //25 cell.textLabel?.text = "test"
+        //25
+        cell.textLabel?.text = typeDizi[indexPath.row]
+        return cell
+    }
+    
+    //30
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsViewController //nereye gidecek
+            destinationVC.selectedType = selecttype
+            destinationVC.selectedId = selectUUID
+            //verileri esitledik
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //31
+        selecttype = typeDizi[indexPath.row]
+        selectUUID = idDizi[indexPath.row]
+        
+        
+        //30
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
 }
 
